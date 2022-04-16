ae5fb0b2e42118b4dd0c4807c3f948c621299cc5
mateiacsinte <matei.acsinte@mincode.ro>
2022-04-16 10:18:10 +0300

    Refactored name tipo

--- src/main/java/com/axi/templates/springcloud/SpringCloudApplication.java
+++ src/main/java/com/axi/templates/springcloud/SpringCloudApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class SpringcloudApplication {
+public class SpringCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringcloudApplication.class, args);
+		SpringApplication.run(SpringCloudApplication.class, args);
 	}
 
 }
